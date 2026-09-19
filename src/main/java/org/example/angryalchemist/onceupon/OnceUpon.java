d3382b658ff665dd4168c838e3398f62e776367b
Maxwell Beals <georgymossman@gmail.com>
2026-09-18 19:20:13 -0700

    Initial commit

---
+++ src/main/java/org/example/angryalchemist/onceupon/OnceUpon.java
@@ -0,0 +1,8 @@
+package org.example.angryalchemist.onceupon;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(OnceUpon.MOD_ID)
+public class OnceUpon {
+    public static final String MOD_ID = "onceupon";
+}
